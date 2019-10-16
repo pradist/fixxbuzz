da3bb31ec8ee148d1d9464f2c2e556c804a92bd0
Pradist <pradist.ke@gmail.com>
2019-10-16 11:55:00 +0700

    Add unit test.

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 func FizzBuzz(input int) string {
+
+	result := ""
+
+	if input%3 == 0 {
+		result += "Fizz"
+	}
+
+	if input%5 == 0 {
+		result += "Buzz"
+	}
+
+	if result != "" {
+		return result
+	}
 	return strconv.Itoa(input)
+}
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		fmt.Println(strconv.Itoa(i)+" : "+FizzBuzz(i))
+	}
+
 }
--- main_test.go
+++ main_test.go
@@ -17,6 +17,14 @@ func TestFizzBuzz(t *testing.T) {
 		{name: "WhenInputIs2_ShouldReturn2", args: args{2}, want: "2"},
 		{name: "WhenInputIs3_ShouldReturnFizz", args: args{3}, want: "Fizz"},
 		{name: "WhenInputIs4_ShouldReturn4", args: args{4}, want: "4"},
+		{name: "WhenInputIs6_ShouldReturnFizz", args: args{6}, want: "Fizz"},
+		{name: "WhenInputIs5_ShouldReturnBuzz", args: args{5}, want: "Buzz"},
+		{name: "WhenInputIs10_ShouldReturnBuzz", args: args{10}, want: "Buzz"},
+		{name: "WhenInputIs15_ShouldReturnFizzBuzz", args: args{15}, want: "FizzBuzz"},
+		{name: "WhenInputIs30_ShouldReturnFizzBuzz", args: args{30}, want: "FizzBuzz"},
+		{name: "WhenInputIs100_ShouldReturnBuzz", args: args{100}, want: "Buzz"},
+		{name: "WhenInputIs90_ShouldReturnFizzBuzz", args: args{90}, want: "FizzBuzz"},
+
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
