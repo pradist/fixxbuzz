afd6ea5c1b8be30afa7ce701a2a2b2d97762e3dd
Pradist <pradist.ke@gmail.com>
2019-10-24 14:00:26 +0700

    FizzBuzz Plus.

--- main.go
+++ main.go
@@ -3,29 +3,43 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func FizzBuzz(input int) string {
 
-	result := ""
+	if Contains(input, "3") && Contains(input, "5") {
+		return "FizzBuzz"
+	}
 
+	if Contains(input, "3") {
+		return "Fizz"
+	}
+
+	if Contains(input, "5") {
+		return "Buzz"
+	}
+
+	if input%15 == 0 {
+		return "FizzBuzz"
+	}
 	if input%3 == 0 {
-		result += "Fizz"
+		return "Fizz"
 	}
 
 	if input%5 == 0 {
-		result += "Buzz"
+		return "Buzz"
 	}
 
-	if result != "" {
-		return result
-	}
 	return strconv.Itoa(input)
+}
+
+func Contains(input int, s string) bool {
+	return strings.Contains(strconv.Itoa(input), s)
 }
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		fmt.Println(strconv.Itoa(i)+" : "+FizzBuzz(i))
+		fmt.Println(strconv.Itoa(i) + " : " + FizzBuzz(i))
 	}
-
 }
--- main_test.go
+++ main_test.go
@@ -20,11 +20,18 @@ func TestFizzBuzz(t *testing.T) {
 		{name: "WhenInputIs6_ShouldReturnFizz", args: args{6}, want: "Fizz"},
 		{name: "WhenInputIs5_ShouldReturnBuzz", args: args{5}, want: "Buzz"},
 		{name: "WhenInputIs10_ShouldReturnBuzz", args: args{10}, want: "Buzz"},
-		{name: "WhenInputIs15_ShouldReturnFizzBuzz", args: args{15}, want: "FizzBuzz"},
-		{name: "WhenInputIs30_ShouldReturnFizzBuzz", args: args{30}, want: "FizzBuzz"},
+		{name: "WhenInputIs15_ShouldReturnBuzz", args: args{15}, want: "Buzz"},
+		{name: "WhenInputIs30_ShouldReturnFizz", args: args{30}, want: "Fizz"},
 		{name: "WhenInputIs100_ShouldReturnBuzz", args: args{100}, want: "Buzz"},
 		{name: "WhenInputIs90_ShouldReturnFizzBuzz", args: args{90}, want: "FizzBuzz"},
-
+		{name: "WhenInputIs13_ShouldReturnFizz", args: args{13}, want: "Fizz"},
+		{name: "WhenInputIs31_ShouldReturnFizz", args: args{31}, want: "Fizz"},
+		{name: "WhenInputIs23_ShouldReturnFizz", args: args{23}, want: "Fizz"},
+		{name: "WhenInputIs51_ShouldReturnBuzz", args: args{51}, want: "Buzz"},
+		{name: "WhenInputIs52_ShouldReturnBuzz", args: args{52}, want: "Buzz"},
+		{name: "WhenInputIs56_ShouldReturnBuzz", args: args{56}, want: "Buzz"},
+		{name: "WhenInputIs35_ShouldReturnFizzBuzz", args: args{35}, want: "FizzBuzz"},
+		{name: "WhenInputIs53_ShouldReturnFizzBuzz", args: args{53}, want: "FizzBuzz"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
